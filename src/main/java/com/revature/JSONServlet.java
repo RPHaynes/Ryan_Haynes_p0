128ce6a263d417636412ded2e5c6a21f7e2ac82c
rphaynes <ryan.haynes@revature.net>
2021-11-18 11:19:38 -0500

    pom

--- src/main/java/com/revature/JSONServlet.java
+++ src/main/java/com/revature/JSONServlet.java
@@ -15,6 +15,11 @@ public class JSONServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try{
+			Worm.getInstance();
+			Worm.closeInstance();
+			resp.getWriter().println("Out");
+		} catch (Exception e) {e.printStackTrace(resp.getWriter());}
 
 	}
 
